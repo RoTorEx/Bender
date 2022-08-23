877c1bb89a4c0518a7f6ba5d732a443a68c4c89c
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-08-23 16:25:25 +0300

    fix: user filter /start

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -7,19 +7,19 @@ from bot.config.config_reader import load_config
 router = Router()
 
 
-@router.message((F.from_user.id.in_(load_config().tg_bot.admin_ids)), commands=["start"])
+@router.message(~(F.from_user.id.in_(load_config().tg_bot.admin_ids)), commands=["start"])
 async def cmd_start(message: Message):
     await message.answer(f"Nice to see you, {message.from_user.first_name}.")
 
 
 @router.message(F.from_user.id.in_(load_config().tg_bot.admin_ids), commands=["start"])
 async def cmd_start_admin(message: Message):
-    await message.answer("Hello, Creator! ^^")
+    await message.answer("Hello, <b>Creator</b>!\n^_^", parse_mode="HTML")
 
 
 @router.message(commands=["name"])
 async def cmd_name(message: types.Message, command: filters.command.CommandObject):
     if command.args:
-        await message.answer(f"Hello, *{command.args}*\!", parse_mode="MarkdownV2")
+        await message.answer(f"Hello, <b>{command.args}</b>!", parse_mode="HTML")
     else:
         await message.answer("Please, enter your name after '/name' command!")
