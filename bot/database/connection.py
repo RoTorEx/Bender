b64f61f9d71315d6afb5577658542bc9995e0dfb
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-08-28 21:43:15 +0300

    refactor: engine generation

--- bot/database/connection.py
+++ bot/database/connection.py
@@ -4,13 +4,24 @@ from sqlalchemy.orm import sessionmaker
 from bot.config.config_reader import Postgres
 
 
-def database_path(db: Postgres):
+def database_path(db: Postgres, async_fallback: bool = False) -> str:
     """Make path to Postgres database."""
     path = f"postgresql+asyncpg://{db.name}:{db.password}@{db.host}:{db.port}/{db.name}"
+
+    if async_fallback:
+        path += "?async_fallback=True"
+
     return path
 
 
-def sa_sessionmarker(db: Postgres):
+def sa_sessionmarker(db: Postgres) -> sessionmaker:
     """Make sessionmaker."""
-    engine = create_async_engine(database_path(db), echo=False)
-    return engine
+    engine = create_async_engine(database_path(db), echo=True)
+
+    return sessionmaker(
+        bind=engine,
+        expire_on_commit=False,
+        class_=AsyncSession,
+        future=True,
+        autoflush=False,
+    )
