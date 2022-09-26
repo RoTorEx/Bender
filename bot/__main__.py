49388ddd41e1be6f03d92b10bcf4ed0465f19708
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-09-26 10:20:20 +0300

    feat: upgrade __main__
    Add connection to Redis as Memory Storage.
    Add dataclass collection.
    Refactor entrypoint -> add try/except.

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,22 +1,31 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.dispatcher.fsm.storage.memory import MemoryStorage
 
 from bot.config.config_reader import read_config
+from bot.config.constants import TgBot
 from bot.config.logger_builder import build_logger
-from bot.database.connection import sa_sessionmaker
-from bot.handlers import commands, messages
+from bot.database.connection import redis_conection, sa_sessionmaker
+from bot.database.reader import get_all_customers
+from bot.handlers import commands, messages_quote_dialog
 from bot.middlewares.db_session import DbSessionMiddleware
 
 
 async def main():
     """Application entrypoint"""
-    logger = build_logger(__name__)
     config = read_config()
+
+    # Choosing Memory Storage
+    if config.tg_bot.use_redis:
+        storage = redis_conection(config.redis)
+
+    else:
+        storage = MemoryStorage()
 
     # Creating bot and its dispatcher
     bot = Bot(token=config.tg_bot.dev_token, parse_mode="HTML")
-    dp = Dispatcher()
+    dp = Dispatcher(storage=storage)
 
     # Creating DB connections pool
     session_pool = sa_sessionmaker(config.postgres, echo=False)
@@ -25,11 +34,16 @@ async def main():
     dp.message.middleware(DbSessionMiddleware(session_pool))
     dp.callback_query.middleware(DbSessionMiddleware(session_pool))
 
+    # Register routers
     dp.include_router(commands.router)
-    dp.include_router(messages.router)
+    dp.include_router(messages_quote_dialog.router)
+
+    # Create contants dataclass
+    TgBot.bot = bot
+    TgBot.admin_list = config.tg_bot.admin_list
+    TgBot.customer_list = await get_all_customers(session_pool)
 
     try:
-        logger.warning("Starting bot...")
         await dp.start_polling(bot)
 
     finally:
@@ -37,4 +51,11 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    logger = build_logger(__name__)
+
+    try:
+        logger.warning("Bot started!")
+        asyncio.run(main())
+
+    except (KeyboardInterrupt, SystemExit):
+        logger.warning("Bot stopped!")
