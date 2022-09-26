c2e6fec7f6a8933489710b17ef311a6d1572be30
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-09-26 10:15:40 +0300

    feat: add Redis connection

--- bot/database/connection.py
+++ bot/database/connection.py
@@ -1,7 +1,8 @@
+from aiogram.dispatcher.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-from bot.config.config_reader import Postgres
+from bot.config.config_reader import Postgres, Redis
 
 
 def database_path(db: Postgres, async_fallback: bool = False) -> str:
@@ -25,3 +26,16 @@ def sa_sessionmaker(db: Postgres, echo: bool = False) -> sessionmaker:
         future=True,
         autoflush=False,
     )
+
+
+def redis_conection(redis: Redis) -> RedisStorage:
+    """Make connection to Redis."""
+    storage = RedisStorage.from_url(
+        url=f"redis://{redis.host}",
+        connection_kwargs={
+            "db": redis.db,
+        },
+        key_builder=DefaultKeyBuilder(with_destiny=True),
+    )
+
+    return storage
